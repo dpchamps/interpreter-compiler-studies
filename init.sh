b1375371801cd4f506efece10425896e0c678f13
gasolinewaltz <gasolinewaltz@gmail.com>
2018-08-04 09:17:47 -0700

    initial commit

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+npm init
+npm install --save-dev babel-core babel-loader babel-minify-webpack-plugin babel-plugin-transform-class-properties babel-plugin-transform-decorators-legacy babel-preset-es2015 babel-preset-minify browser-sync browser-sync-webpack-plugin gulp uglifyjs-webpack-plugin webpack webpack-cli webpack-dev-middleware webpack-dev-server webpack-hot-middleware
+npm install lodash
+npm build
