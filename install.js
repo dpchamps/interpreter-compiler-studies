3ad583630dca1305fdf87939e62b45af20afb30a
gasolinewaltz <gasolinewaltz@gmail.com>
2018-08-04 10:51:20 -0700

    added entry and license, build script

--- install.js
+++ install.js
@@ -7,6 +7,8 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+const licenseFile = 'license.md';
+const packageFile = 'package.json';
 
 const prompt = async (question, defaultAnswer = "") => {
     return new Promise(resolve => {
@@ -19,21 +21,41 @@ const prompt = async (question, defaultAnswer = "") => {
         })
     });
 };
+
+const writeProject = async () => {
+    project.name = await prompt('Project Name');
+    project.version = await prompt('Version (0.0.1)', '0.0.1');
+    project.author = await prompt('Author Name');
+    project.license = await prompt('License Type (MIT)', 'MIT');
+
+    fs.writeFileSync(`${__dirname}/package.json`, JSON.stringify(project, null, 1));
+    return Promise.resolve();
+};
+
+const writeLicense = async () => {
+    const licenseName = await prompt('Author this project is licensed under', '');
+    const copyrightLine = `Copyright (c) [${(new Date()).getFullYear()}] [${licenseName}]`;
+    const license = fs.readFileSync(`${__dirname}/license.md`, 'utf8').split("\n");
+
+    license[2] = copyrightLine;
+
+    fs.writeFileSync(`${__dirname}/${licenseFile}`, license.join("\n"));
+    return Promise.resolve();
+};
+
  (async() => {
      try{
-         project.name = await prompt('Project Name');
-         project.version = await prompt('Version (0.0.1)', '0.0.1');
-         project.author = await prompt('Author Name');
-         project.license = await prompt('License Type (MIT)', 'MIT');
+         await writeProject();
+         await writeLicense();
 
          console.log('Install complete! Running `npm build`...');
+
          execSync('npm run build', {stdio:[0,1,2]});
-         fs.writeFileSync(`${__dirname}/package.json`, JSON.stringify(project, null, 1));
          fs.unlinkSync(`${__dirname}/install.js`);
          console.log('Build Finished :D');
          process.exit(0);
      }catch(error){
-         console.log('There was an error in the postinstall script D:')
+         console.log('There was an error in the postinstall script D:');
          process.exit(1);
      }
      
---
+++ readme.md
@@ -0,0 +1,12 @@
+#Barebones WebPack boilerplate for writing libraries
+
+##Usage:
+
+```javascript
+git clone https://github.com/gasolinewaltz/webpack-boilerplate.git <project-name>
+cd <project-name>
+npm install
+```
+
+##Details:
+
