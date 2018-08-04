132da38b0a31ee6ec75952cd6458dedc134eb21d
gasolinewaltz <gasolinewaltz@gmail.com>
2018-08-04 09:25:21 -0700

    It should remove existing git repo and shell script after build

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
+rm -rf ./.git
+git init
 npm init
 npm install --save-dev babel-core babel-loader babel-minify-webpack-plugin babel-plugin-transform-class-properties babel-plugin-transform-decorators-legacy babel-preset-es2015 babel-preset-minify browser-sync browser-sync-webpack-plugin gulp uglifyjs-webpack-plugin webpack webpack-cli webpack-dev-middleware webpack-dev-server webpack-hot-middleware
 npm install lodash
 npm build
+rm ./init.sh
